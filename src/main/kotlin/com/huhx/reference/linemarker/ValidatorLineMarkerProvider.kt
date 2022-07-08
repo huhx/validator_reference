1d66b5b7f474e3acc136df11a01ce449df1559cd
hongxiang <huxhu@thoughtworks.com>
2022-07-08 20:07:15 +0800

    [huhx] -feat: add Line Marker provider for the MethodValidator

---
+++ src/main/kotlin/com/huhx/reference/linemarker/ValidatorLineMarkerProvider.kt
@@ -0,0 +1,44 @@
+package com.huhx.reference.linemarker
+
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider
+import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiNameValuePair
+import com.intellij.psi.PsiReferenceExpression
+import com.intellij.psi.impl.search.JavaFilesSearchScope
+import com.intellij.psi.search.PsiShortNamesCache
+import com.huhx.reference.AppSettingsState
+
+class ValidatorLineMarkerProvider : RelatedItemLineMarkerProvider() {
+    var className: String = AppSettingsState.getInstance().className
+
+    override fun collectNavigationMarkers(element: PsiElement, result: MutableCollection<in RelatedItemLineMarkerInfo<*>>) {
+        if (element !is PsiReferenceExpression || element.parent !is PsiNameValuePair) {
+            return
+        }
+
+        if (!element.text.startsWith(className)) {
+            return
+        }
+
+        val properties = findProperties(element.project, element.referenceName!!)
+        if (properties.isNotEmpty()) {
+            val lineMarkerInfo = NavigationGutterIconBuilder.create(AllIcons.Chooser.Left)
+                .setTargets(properties)
+                .setTooltipText("Navigate to MethodValidate")
+                .createLineMarkerInfo(element)
+            result.add(lineMarkerInfo)
+        }
+    }
+
+    private fun findProperties(project: Project, referenceName: String): List<PsiElement> {
+        val methodName = "@ValidationMethod(%s)".format(referenceName)
+        val psiClasses = PsiShortNamesCache.getInstance(project).getClassesByName(className, JavaFilesSearchScope(project))
+        return psiClasses[0].methods.filter {
+            it.text.contains(methodName)
+        }
+    }
+}
