ba664507aec9ba587a62678a45fce442ab130923
hongxiang <huxhu@thoughtworks.com>
2022-07-09 20:10:17 +0800

    [huhx] -refactor: create getPsiClasses extension method for Project

--- src/main/kotlin/com/huhx/reference/extension/ProjectExtension.kt
+++ src/main/kotlin/com/huhx/reference/extension/ProjectExtension.kt
@@ -3,21 +3,23 @@ package com.huhx.reference.extension
 import com.huhx.reference.constant.Constant
 import com.huhx.reference.setting.AppSettingsState
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.search.JavaFilesSearchScope
 import com.intellij.psi.search.PsiShortNamesCache
 
 
 fun Project.findPsiElement(value: String): List<PsiElement> {
-    val className: String = AppSettingsState.getInstance().className
-    val psiClasses = PsiShortNamesCache.getInstance(this).getClassesByName(className, JavaFilesSearchScope(this))
-    return psiClasses[0].methods.filter {
+    return getPsiClasses().first().methods.filter {
         it.hasAnotationValue(Constant.VALIDATION_METHOD_NAME, value)
     }
 }
 
 fun Project.hasReference(value: String): Boolean {
+    return getPsiClasses().first().findFieldByName(value.uppercase(), false) != null
+}
+
+private fun Project.getPsiClasses(): Array<out PsiClass> {
     val className: String = AppSettingsState.getInstance().className
-    val psiClass = PsiShortNamesCache.getInstance(this).getClassesByName(className, JavaFilesSearchScope(this)).first()
-    return psiClass.findFieldByName(value.uppercase(), false) != null
+    return PsiShortNamesCache.getInstance(this).getClassesByName(className, JavaFilesSearchScope(this))
 }
