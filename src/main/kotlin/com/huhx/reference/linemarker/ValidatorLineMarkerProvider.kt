aea13f7b253bab8dc08752f2c4c270375062f055
hongxiang <huxhu@thoughtworks.com>
2022-07-11 12:56:01 +0800

    [huhx] -feat: fix the issue that can not work in cases of static import in ValidatorLineMarkerProvider

--- src/main/kotlin/com/huhx/reference/linemarker/ValidatorLineMarkerProvider.kt
+++ src/main/kotlin/com/huhx/reference/linemarker/ValidatorLineMarkerProvider.kt
@@ -1,29 +1,21 @@
 package com.huhx.reference.linemarker
 
+import com.huhx.reference.constant.Constant.METHOD_VALIDATION_NAME
 import com.huhx.reference.extension.findPsiElement
-import com.huhx.reference.setting.AppSettingsState
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
 import com.intellij.icons.AllIcons
+import com.intellij.psi.PsiAnnotation
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiNameValuePair
-import com.intellij.psi.PsiReferenceExpression
 
 class ValidatorLineMarkerProvider : RelatedItemLineMarkerProvider() {
 
     override fun collectNavigationMarkers(element: PsiElement, result: MutableCollection<in RelatedItemLineMarkerInfo<*>>) {
-        val className: String = AppSettingsState.getInstance().className
-
-        if (element !is PsiReferenceExpression || element.parent !is PsiNameValuePair) {
+        if (element !is PsiAnnotation || !element.nameReferenceElement?.qualifiedName.equals(METHOD_VALIDATION_NAME)) {
             return
         }
-
-        if (!element.text.startsWith(className)) {
-            return
-        }
-
-        val referenceName = element.referenceName ?: return
+        val referenceName = element.findAttributeValue("method")?.lastChild?.text ?: return
 
         val properties = element.project.findPsiElement(referenceName)
         if (properties.isNotEmpty()) {
@@ -33,6 +25,5 @@ class ValidatorLineMarkerProvider : RelatedItemLineMarkerProvider() {
                 .createLineMarkerInfo(element)
             result.add(lineMarkerInfo)
         }
-
     }
 }
