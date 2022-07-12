8dd16bc4a4a3861fe3cefd0c21f33c1f58970abf
hongxiang <huxhu@thoughtworks.com>
2022-07-12 23:04:59 +0800

    [huhx] -feat: opmitize the navigation use keyshort

--- src/main/kotlin/com/huhx/reference/action/MethodAction.kt
+++ src/main/kotlin/com/huhx/reference/action/MethodAction.kt
@@ -1,13 +1,12 @@
 package com.huhx.reference.action
 
-import com.huhx.reference.constant.Constant.ANNOTATION_NAME
+import com.huhx.reference.constant.Constant.METHOD_VALIDATION_NAME
 import com.huhx.reference.extension.findPsiElement
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.psi.PsiAnnotation
-import com.intellij.psi.PsiIdentifier
 import com.intellij.psi.PsiManager
 import com.intellij.psi.util.parentOfType
 import com.intellij.util.PsiNavigateUtil
@@ -20,12 +19,14 @@ class MethodAction : AnAction() {
         val virtualFile = FileDocumentManager.getInstance().getFile(editor.document) ?: return
 
         val offset = editor.caretModel.offset
-        val element = PsiManager.getInstance(project).findFile(virtualFile)?.findElementAt(offset)
-        if (element !is PsiIdentifier || element.text != ANNOTATION_NAME) {
+        val element = PsiManager.getInstance(project).findFile(virtualFile)?.findElementAt(offset) ?: return
+
+        val psiAnnotation = element.parentOfType<PsiAnnotation>(true)
+        if (psiAnnotation?.qualifiedName != METHOD_VALIDATION_NAME) {
             return
         }
 
-        val attributeValueElement = element.parentOfType<PsiAnnotation>()?.findAttributeValue("method")?.lastChild
+        val attributeValueElement = psiAnnotation.findAttributeValue("method")?.lastChild
         attributeValueElement?.let {
             val psiElements = project.findPsiElement(attributeValueElement.text)
             PsiNavigateUtil.navigate(psiElements.first(), true)
